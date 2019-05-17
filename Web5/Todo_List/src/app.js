cc966c703552c758611738c6d6ff2523cdfa687f
Francisco Payan Calero <fpayan.calero@gmail.com>
2019-05-17 19:00:48 +0200

    Added folder Web5/Todo_List. This folder contains the entire project Todo_List. This is base boilerplate of proyect Todo_List

---
+++ Web5/Todo_List/src/app.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+import './scss/app.scss';
+
+$('document').ready(function(){
+    console.log('Ready Hi.. Updating.. ;-) \\O/ ');
+
+    $('#btnTest').click(function(){
+        console.log('Click in button with bootstrap 4. update');
+    });
+});
